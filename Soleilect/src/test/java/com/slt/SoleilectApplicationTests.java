1d5f086a96838aeef99c14bb143e17c48ebc94f0
naraeha <dkfmrhs170@gmail.com>
2024-02-05 14:26:26 +0900

    프로젝트 생성

---
+++ Soleilect/src/test/java/com/slt/SoleilectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleilectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
