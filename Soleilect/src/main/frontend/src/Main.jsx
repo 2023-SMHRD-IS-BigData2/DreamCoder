2d31ad8f2e57f9a7feba77e32ab944bc5853be9a
장서영 <145406855+seoyoung430@users.noreply.github.com>
2024-02-06 14:24:30 +0900

    장서영-test
    test ㅡMain.jsx에 작성했어요

--- Soleilect/src/main/frontend/src/Main.jsx
+++ Soleilect/src/main/frontend/src/Main.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
+
   return (
-    <div>Main</div>
+    <div>Main
+      <form action='/Sol/joinCon/join'>
+       아이디 : <input type='text' name='id' > </input><br/>
+       비번 : <input type='text' name='pw'> </input><br/>
+       이름 : <input type='text' name='name'> </input><br/>
+       사업자번호 : <input type='text' name='b_num'> </input><br/>
+        <input type='submit' value='전송'></input>
+      </form >
+    </div>
   )
 }
 
