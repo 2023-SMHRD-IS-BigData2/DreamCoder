f47be5ca4119a5f892eb71cb7217e0e0e8eec40f
naraeha <dkfmrhs170@gmail.com>
2024-02-06 15:59:29 +0900

    회원가입 구현

--- Soleilect/src/main/frontend/src/Main.jsx
+++ Soleilect/src/main/frontend/src/Main.jsx
@@ -1,10 +1,32 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
 
+  const nav = useNavigate();
+
+  const submitPost = (e)=>{
+    let formData = new FormData();
+    formData.append("id",e.target.id.value)
+    formData.append("pw",e.target.pw.value)
+    formData.append("nickname",e.target.nickname.value)
+    formData.append("name",e.target.name.value)
+    formData.append("b_num",e.target.b_num.value)
+    axios
+    .post('/Sol/joinCon/join',formData)
+    .then((response)=>{
+      console.log(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+    nav('/')
+  }
+
   return (
     <div>Main
-      <form action='/Sol/joinCon/join'>
+      <form onSubmit={submitPost}>
        아이디 : <input type='text' name='id' ></input><br/>
        비번 : <input type='text' name='pw'></input><br/>
        닉네임 : <input type='text' name='nickname'></input><br/>
