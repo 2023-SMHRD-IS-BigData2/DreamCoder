6837ab57660ef9c86d8094d5b77d6a5529cd714d
강지민 <145407345+KHihello@users.noreply.github.com>
2024-02-06 16:59:18 +0900

    Security기본설정준비(아직 적용안함)

--- Soleilect/src/main/java/com/slt/config/SecurityConfig.java
+++ Soleilect/src/main/java/com/slt/config/SecurityConfig.java
@@ -1,5 +1,19 @@
 package com.slt.config;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				// HttpServletRequest를 사용하는 요청들에 대한 접근 설정
+				.authorizeRequests()
+				// /api/hello에 대한 요청은 인증없이 접근을 허용
+				.antMatchers("/").permitAll()
+				// 나머지 요청들은 모두 인증되어야 한다는 의미
+				.anyRequest().authenticated();
+	}
 
 }
