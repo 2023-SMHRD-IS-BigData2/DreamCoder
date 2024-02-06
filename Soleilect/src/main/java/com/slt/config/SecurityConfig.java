e242362f97a3dffa83164605fd6740426d3d62d2
강지민 <145407345+KHihello@users.noreply.github.com>
2024-02-06 17:26:46 +0900

    SecurityConfig 일부 수정

--- Soleilect/src/main/java/com/slt/config/SecurityConfig.java
+++ Soleilect/src/main/java/com/slt/config/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http
 				// HttpServletRequest를 사용하는 요청들에 대한 접근 설정
 				.authorizeRequests()
-				// /api/hello에 대한 요청은 인증없이 접근을 허용
+				// /에 대한 요청은 인증없이 접근을 허용
 				.antMatchers("/").permitAll()
 				// 나머지 요청들은 모두 인증되어야 한다는 의미
 				.anyRequest().authenticated();
