e0dea44bcea212b478da6b335f0c56849a9e788e
naraeha <dkfmrhs170@gmail.com>
2024-02-07 11:56:39 +0900

    Revert "회원가입/로그인 페이지 구현"
    This reverts commit c511529773fe7b2618f4e1f8e9dd62905ad58c23.

---
+++ Soleilect/src/main/frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+import { Route, Routes } from 'react-router-dom';
+import axios from "axios";
+import './App.css';
+import { ChartContext } from "./context/ChartContext";
+import Header from "./layouts/Header/Header";
+import Main from './Main';
+import ChartList from './components/Chart/ChartList';
+import ChartDetail from './components/Chart/ChartDetail';
+
+function App() {
+  
+  const [data, setData] =useState('');
+  
+  // 모집 게시판 useState
+  const[list,setList] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/Sol/test')
+    .then(res=>setData(res.data))
+    .catch(err=>console.log(err))
+  },[])
+
+
+  return (
+    <ChartContext.Provider value={{list, setList}}>
+    <div>
+      <Header/>
+      <Routes>
+        <Route path="/Main" element={<Main/>}></Route>
+        <Route path="/ChartList" element={<ChartList/>}></Route>
+        <Route path='/detail/:num' element={<ChartDetail/>}></Route>
+      </Routes>
+    </div>
+    </ChartContext.Provider>
+  );
+}
+
+export default App;
---
+++ Soleilect/src/main/frontend/src/components/Chart/ChartItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChartItem = () => {
+  return (
+    <div>ChartItem</div>
+  )
+}
+
+export default ChartItem
