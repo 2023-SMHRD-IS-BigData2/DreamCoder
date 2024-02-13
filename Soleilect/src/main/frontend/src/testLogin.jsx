3d015b83dea60c5f1f03539bbc4dd28f4093b1e5
naraeha <dkfmrhs170@gmail.com>
2024-02-13 11:48:53 +0900

    testLogin 페이지 추가

--- Soleilect/src/main/frontend/src/testLogin.jsx
+++ Soleilect/src/main/frontend/src/testLogin.jsx
@@ -10,17 +10,14 @@ const Main = () => {
     let formData = new FormData();
     formData.append("user_id",e.target.user_id.value)
     formData.append("user_pw",e.target.user_pw.value)
-    formData.append("user_nick",e.target.user_nick.value)
-    formData.append("user_name",e.target.user_name.value)
     axios
-    .post('/Sol/joinCon/join',formData)
+    .post('/Sol/logCon/login',formData)
     .then((response)=>{
       console.log(response.data)
     })
     .catch((error)=>{
       console.log(error)
     })
-    nav('/');
   
   }
 
@@ -29,8 +26,6 @@ const Main = () => {
       <form onSubmit={submitPost}>
        아이디 : <input type='text' name='user_id'></input><br/>
        비번 : <input type='text' name='user_pw'></input><br/>
-       닉네임 : <input type='text' name='user_nick'></input><br/>
-       이름 : <input type='text' name='user_name'></input><br/>
         <input type='submit' value='전송'></input>
       </form >
     </div>
