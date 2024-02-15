4697d672605e2ad13a931151d46a11839acc5420
naraeha <dkfmrhs170@gmail.com>
2024-02-15 12:44:58 +0900

    마이페이지 발전소 삽입, 불러오기, 일반게시판 댓글 삽입, 불러오기 구현

--- Soleilect/src/main/java/com/slt/service/MyPageService.java
+++ Soleilect/src/main/java/com/slt/service/MyPageService.java
@@ -1,5 +1,29 @@
 package com.slt.service;
 
-public class MyPageService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.slt.entity.Plants;
+import com.slt.mapper.MyPageMapper;
+
+@Service
+public class MyPageService {
+	
+	@Autowired
+	private MyPageMapper mypagemapper;
+	
+	public int plantInsert(Plants plants) {
+		int row = mypagemapper.plantInsert(plants);
+		
+		return row;
+	}
+	
+	
+	public List<Plants> plantList(String id){
+		List<Plants> list = mypagemapper.plantList(id);
+		
+		return list;
+	}
 }
