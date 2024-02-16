04256952b2342d64db6ec907ae4e55df543070bd
naraeha <dkfmrhs170@gmail.com>
2024-02-16 10:00:11 +0900

    어노테이션 수정

--- Soleilect/src/main/java/com/slt/service/PartyCommentService.java
+++ Soleilect/src/main/java/com/slt/service/PartyCommentService.java
@@ -3,10 +3,12 @@ package com.slt.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.slt.entity.Party_comments;
 import com.slt.mapper.PartyCommentMapper;
 
+@Service
 public class PartyCommentService {
 	
 	@Autowired
