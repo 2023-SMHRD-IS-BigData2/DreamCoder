607911fed84b2ae4964ac33575b16bd890d4c25c
손지우 <145410812+sonjiu00@users.noreply.github.com>
2024-02-21 18:28:23 +0900

    수리업체 페이징구현

--- Soleilect/src/main/frontend/src/components/Pagination/Pagination.jsx
+++ Soleilect/src/main/frontend/src/components/Pagination/Pagination.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import './style.css';
 
 const Pagination = ({ currentPage, totalPages, onNextPage, onPrevPage, onGoToPage }) => {
     const startPage = Math.floor((currentPage - 1) / 10) * 10 + 1; // 페이지네이션 시작 페이지
     const endPage = Math.min(startPage + 9, totalPages); // 페이지네이션 끝 페이지
 
     return (
-        <div className='pagination'>
-            <button onClick={onPrevPage} disabled={currentPage === 1}>{'<'}</button>
-            <span>{`Page ${currentPage} of ${totalPages}`}</span>
-            <button onClick={onNextPage} disabled={currentPage === totalPages}>{'>'}</button>
-            <div>
-                {/* 페이지 번호를 표시하는 버튼들 */}
-                {Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index).map((page) => (
-                    <button key={page} onClick={() => onGoToPage(page)} className={currentPage === page ? 'active' : ''}>
-                        {page}
-                    </button>
-                ))}
+        <div id='pagination-wrapper'>
+            <div className='pagination-change-link-box'>
+                <div className='icon-box-small'>
+                    <div className='icon expand-left-icon'></div>
+                </div>
+                <div className='pagination-change-link-text' onClick={onPrevPage} disabled={currentPage === 1}>{'이전'}</div>
+            </div>
+            <div className='pagination-divider'>{'\|'}</div>
+
+            {/* 페이지 번호를 표시하는 버튼들 */}
+            {Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index).map((page) => (
+                <div className={currentPage === page ? 'pagination-text-active' : 'pagination-text'} key={page} onClick={() => onGoToPage(page)}>
+                    {page}
+                </div>
+            ))}
+
+            <div className='pagination-divider'>{'\|'}</div>
+            <div className='pagination-change-link-text' onClick={onNextPage} disabled={currentPage === totalPages}>{'다음'}</div>
+            <div className='pagination-change-link-box'>
+                <div className='icon-box-small'>
+                    <div className='icon expand-right-icon'></div>
+                </div>
             </div>
         </div>
     );
