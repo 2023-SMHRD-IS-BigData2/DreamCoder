7d8e16e1bcf568ca189ae2307100438192f16b48
장서영 <145406855+seoyoung430@users.noreply.github.com>
2024-02-22 08:58:55 +0900

    페이징 추가

--- Soleilect/src/main/frontend/src/views/PartyBoard/PartyBoardList.jsx
+++ Soleilect/src/main/frontend/src/views/PartyBoard/PartyBoardList.jsx
@@ -11,7 +11,7 @@ const PartyBoardList = () => {
     const nav = useNavigate();
 
      // 페이지네이션 훅 호출
-    //  const { currentPage, currentData, totalPageCount, nextPage, prevPage, goToPage } = usePagination(6, list); 
+     const { currentPage, currentData, totalPageCount, nextPage, prevPage, goToPage } = usePagination(6, list); 
 
   // 스프링부트 연결 ---------------------
   useEffect(()=>{
@@ -37,22 +37,22 @@ const PartyBoardList = () => {
         }}>{'모집하러 가기'}</div>
       </div>
       <div className='list-item'>
-        {/* {list && currentData() && currentData().map((item, index) => <PartyBoardItem item={item}
-          key={(currentPage - 1) * 6 + index} index={index}></PartyBoardItem>)} */}
-           {list.map((item, index) => <PartyBoardItem item={item}
-          key={index} index={index}></PartyBoardItem>)}
+        {list && currentData() && currentData().map((item, index) => <PartyBoardItem item={item}
+          key={(currentPage - 1) * 6 + index} index={index}></PartyBoardItem>)}
+           {/* {list.map((item, index) => <PartyBoardItem item={item}
+          key={index} index={index}></PartyBoardItem>)} */}
       </div>
       <br></br>
             <div className='repair-info-bottom-box'>
                     <div className='info-list-pagination-box'>
                         {/* 페이지네이션 컴포넌트 */}
-                        {/* <Pagination
+                        <Pagination
                             currentPage={currentPage}
                             totalPages={totalPageCount}
                             onNextPage={nextPage}
                             onPrevPage={prevPage}
                             onGoToPage={goToPage}
-                        /> */}
+                        />
                     </div>
                 </div>
     </div>
