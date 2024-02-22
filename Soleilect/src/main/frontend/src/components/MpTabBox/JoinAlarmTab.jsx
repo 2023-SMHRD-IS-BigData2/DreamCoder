9bb3602c5fa0aa394820e735456d530c873da43c
손지우 <145410812+sonjiu00@users.noreply.github.com>
2024-02-22 15:58:51 +0900

    알림내역-신청알림리스트연동

--- Soleilect/src/main/frontend/src/components/MpTabBox/JoinAlarmTab.jsx
+++ Soleilect/src/main/frontend/src/components/MpTabBox/JoinAlarmTab.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 const JoinAlarmTab = (props) => {
-    const { target_cnt, party_title, start_at, end_at, party_content, now_cnt, index } = props;
+    const { target_cnt, party_title, start_at, end_at, party_content, now_cnt, index,user_nick,pl_power,pl_name } = props;
     return (
         <div className='tab-content-list'>
             <div className='tab-content-box'>
@@ -14,31 +14,31 @@ const JoinAlarmTab = (props) => {
                 </div>
                 <div className='alarm-tab-text-content-box'>
                     <div className='tab-top-text-box'>
-                        <div className='border-text-box'>
+                        {/* <div className='border-text-box'>
                             <div className='alarm-tab border-text'>{target_cnt / 1000 + 'MW'}</div>
-                        </div>
+                        </div> */}
                         <div className='content-name'>{party_title}</div>
                     </div>
-                    <div className='tab-bottom-text-box'>
+                    {/* <div className='tab-bottom-text-box'>
                         <div className='bottom-text'>{start_at + '-' + end_at}</div>
-                    </div>
+                    </div> */}
                 </div>
                 <div class='v-line'></div>
                 <div className='alarm-tab-second-text-content-box'>
                     <div className='tab-top-second-text-box'>
-                        <div className='second-content-name'>{'닉네임'}</div>
-                        <div className='second-bottom-text'>{'2024.02.15'}</div>
+                        <div className='second-content-name'>{user_nick}</div>
+                        {/* <div className='second-bottom-text'>{'2024.02.15'}</div> */}
                     </div>
                     <div className='second-inner-box'>
                         <div className='second-inner-box-left'>
                             <div className='second-inner-box-left-top'>
-                                <div className='second-inner-box-text'>{'발전량'}</div>
+                                <div className='second-inner-box-text'>{pl_power}</div>
                                 <div className='second-inner-box-text-box'>
-                                    <div className='second-inner-box-text'>{'발전소이름'}</div>
+                                    <div className='second-inner-box-text'>{pl_name}</div>
                                 </div>
                             </div>
                             <div className='second-inner-box-left-bottom'>
-                                <div className='second-inner-box-text'>{'발전소 위치'}</div>
+                                {/* <div className='second-inner-box-text'>{'발전소 위치'}</div> */}
                             </div>
                         </div>
                     </div>
