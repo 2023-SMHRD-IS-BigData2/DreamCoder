1f20fb9bb0d481eac3795483be1719dfab4bac60
손지우 <145410812+sonjiu00@users.noreply.github.com>
2024-02-22 23:37:46 +0900

    메인페이지수정

--- Soleilect/src/main/frontend/src/views/Main/Main.jsx
+++ Soleilect/src/main/frontend/src/views/Main/Main.jsx
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import './main.css';
+import {useNavigate } from 'react-router-dom';
 
 const Main = () => {
+  const nav = useNavigate();
+  //        event handler: 시작하기 누르면 회원가입창으로 감
+  const goToJoinIn=()=>{
+    nav('/SignUp')
+  }
 
   return (
     <div className='main-wrapper'>
@@ -17,6 +23,7 @@ const Main = () => {
               <div className='main-cover-text-grey'>필요한 만큼의 전력을 쉽고</div>
               <div className='main-cover-text-grey'>간단하게 모아보세요!</div>
             </div>
+            <div className='main-cover-button' onClick={goToJoinIn}>시작하기</div>
           </div>
         </div>
       </div>
