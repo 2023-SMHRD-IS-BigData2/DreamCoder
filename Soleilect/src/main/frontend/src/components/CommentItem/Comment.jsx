4fa893377b72e07bb28bd13701b2e9e790ad66ee
장서영 <145406855+seoyoung430@users.noreply.github.com>
2024-02-23 09:51:40 +0900

    댓글 조회, 등록 연동

--- Soleilect/src/main/frontend/src/components/CommentItem/Comment.jsx
+++ Soleilect/src/main/frontend/src/components/CommentItem/Comment.jsx
@@ -5,14 +5,18 @@ import { ChartContext } from '../../context/ChartContext';
 const Comment = (props) => {
     //  State 댓글 목록 상태
     const [comments, setComments] = useState([]);
+    //  state 댓글 삭제 상태
+    const [commentsDelelte,setCommentsDelete] = useState([]);
+    //  state 댓글 등록 상태
+    const [commentsInsert,setCommentsInsert] = useState([]);
 
-    const {party_seq, user_id, user_nick} = props;
+    const { party_seq, user_id, user_nick } = props;
 
     // state 댓글 상태 
-    const [p_cmt_ref,set_p_cmt_ref] = useState(1);
-    const [p_cmt_ref_level,set_p_cmt_ref_level] = useState(1);
-    const [p_cmt_parent,set_p_cmt_parent] = useState(1);
-    const [p_cmt_content,set_p_cmt_content] = useState("");
+    const [p_cmt_ref, set_p_cmt_ref] = useState(1);
+    const [p_cmt_ref_level, set_p_cmt_ref_level] = useState(1);
+    const [p_cmt_parent, set_p_cmt_parent] = useState(1);
+    const [p_cmt_content, set_p_cmt_content] = useState("");
 
     // 내용 요소 참조 상태
     const contentRef = useRef(null);
@@ -30,94 +34,96 @@ const Comment = (props) => {
     //     setComments(updatedComments);
     // };
 
-    useEffect(()=>{
+    // 백엔드에서 댓글 목록 가져오기 -----------
+    useEffect(() => {
         let formData = new FormData();
         console.log(party_seq);
-        formData.append("party_seq",party_seq)
-    axios
-    .post('/Sol/partyCommentCon/list',formData)
-    .then((res)=>{
-        setComments(res.data.data)
-        console.log(res);
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-    },[])
+        formData.append("party_seq", party_seq)
+        axios
+            .post('/Sol/partyCommentCon/list', formData)
+            .then((res) => {
+                setComments(res.data.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        console.log(comments);
+    }, [])
 
     // event handler  : 댓글 등록 클릭 이벤트-------------------------------
     const onCommentsSubmitClickHandler = () => {
         console.log('실행');
-        {commentsSubmit()}
-        
+        { commentsSubmit() }
+
     }
     // function : 댓글 등록 백엔드 연결
     const commentsSubmit = () => {
-        let formaData =  new FormData();
-        formaData.append("party_seq",party_seq)
-        formaData.append("p_cmt_ref",1)
-        formaData.append("p_cmt_ref_level",1)
-        formaData.append("p_cmt_content",p_cmt_content)
-        formaData.append("p_cmt_parent",1)
-        formaData.append("user_id",user_id)
-        formaData.append("user_nick",user_nick)
+        let formaData = new FormData();
+        formaData.append("party_seq", party_seq)
+        formaData.append("p_cmt_ref", 1)
+        formaData.append("p_cmt_ref_level", 1)
+        formaData.append("p_cmt_content", p_cmt_content)
+        formaData.append("p_cmt_parent", 1)
+        formaData.append("user_id", user_id)
+        formaData.append("user_nick", user_nick)
         axios
-        .post('/Sol/partyCommentCon/insert',formaData)
-        .then((res)=>{
-            setComments(res.data.data)
-            console.log(res.data.data);
-            console.log('댓글등록 성공');
-        })
+            .post('/Sol/partyCommentCon/insert', formaData)
+            .then((res) => {
+                setCommentsInsert(res.data.data)
+                console.log(res.data.data);
+                console.log('댓글등록 성공');
+            })
 
     }
 
     // event handler  : 댓글 삭제 클릭 이벤트-------------------------------
-    const onCommentsDeleteClickHandler = () => {
-        {commentsDelete()}
+    const onCommentsDeleteClickHandler = (delete_num) => {
+        console.log('클릭');
+        console.log(delete_num);
+        { commentsDelete(delete_num) }
     }
     // function : 댓글 삭제 백엔드 연결
-    const commentsDelete = () => {
-        let formaData =  new FormData();
-        formaData.append("p_cmt_seq")
+    const commentsDelete = (delete_num) => {
+        let formaData = new FormData();
+        formaData.append("p_cmt_seq",delete_num)
         axios
-        .post('/Sol/partyCommentCon/delete',formaData)
-        .then((res)=>{
-            setComments(res.data.data)
-            console.log(res.data.data);
-            console.log('댓글삭제 성공');
-        })
+            .post('/Sol/partyCommentCon/delete', formaData)
+            .then((res) => {
+                setCommentsDelete(res.data.data)
+                console.log(res.data.data);
+                console.log('댓글삭제 성공');
+            })
     }
 
     return (
         <div className="comment-container">
             <h2>댓글</h2>
+
+            {comments.map((item,index)=>(
             <div className='comment-box'>
-                <ul className="comment-list"> 
-                    
-                        
-                        {/* <li key={commentIndex}> */}
-                            <div className="comment-header">
-                                <div className='board-detail-writer-profile-image'></div>
-                                <div className='board-detail-writer-profile-conten'>
-                                    {/* <span className="comment-username">{comment.username}</span> */}
-                                    {/* <div className="comment-text">{comment.text}</div> */}
-                                </div>
-                                {/* <div onClick={() => onCommentsDeleteClickHandler(commentIndex)} className='comment-text-delete' >{'삭제'}</div> */}
-                            </div>
-                            <div className='comment-bottom-box'>
-                            <div className='comment-bottom-date'>{'2024-02-16'}</div>
-                            {/* <div className='comment-text-write-reply'>{'답글쓰기'}</div> */}
-                            </div>
-                            <div className='divider-comment'></div>
+                <ul className="comment-list">
+                    <li>
+                    <div className="comment-header">
+                        <div className='board-detail-writer-profile-image'></div>
+                        <div className='board-detail-writer-profile-conten'>
+                             <span className="comment-username">{item.user_nick}</span> 
+                            <div className="comment-text">{item.p_cmt_content}</div>
+                        </div>
+                        <div onClick={() => onCommentsDeleteClickHandler(item.p_cmt_seq)} className='comment-text-delete' >{'삭제'}</div>
+                    </div>
+                    <div className='comment-bottom-box'>
+                        <div className='comment-bottom-date'>{'2024-02-16'}</div>
+                    </div>
+                    <div className='divider-comment'></div>
 
-                        {/* </li> */}
-                    
-
+                    </li>
                 </ul>
             </div>
+))}
+
             <div className='comment-new-box'>
                 <form>
-                    <input type="text" name="p_cmt_content" placeholder="댓글 입력" onChange={onCommentContentChangeHandler} ref={contentRef} value={p_cmt_content}/>
+                    <input type="text" name="p_cmt_content" placeholder="댓글 입력" onChange={onCommentContentChangeHandler} ref={contentRef} value={p_cmt_content} />
                     <button type="submit" onClick={onCommentsSubmitClickHandler} >댓글 등록</button>
                 </form>
             </div>
