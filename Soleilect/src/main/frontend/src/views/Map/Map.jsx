98a7eb30e1240f7aae293127a6a26d99e147973d
장서영 <145406855+seoyoung430@users.noreply.github.com>
2024-02-24 10:45:25 +0900

    맵 연동 중

--- Soleilect/src/main/frontend/src/views/Map/Map.jsx
+++ Soleilect/src/main/frontend/src/views/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect ,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Map.css';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
@@ -9,221 +9,216 @@ const Map = () => {
 
   // state plantDeep 연결 상태  
   const [plantList, setPlantList] = useState([]);
+  // state highcharts options 상태
+  const [options, setOptions] = useState(null);  
+
+  // 백엔드에서 발전량 정보 가져오기
+  useEffect(() => {
+    let formData = new FormData();
+    axios
+      .get('/Sol/mapCon/sum', formData)
+      .then((res) => {
+        setPlantList(res.data.data)
+
+        // 지도 여는 함수 실행
+        mapMarkerInfoWindow(res.data.data);    
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []);
+
+  // test 인포메이션 ---------------------------------------
+  // state 발전소 이름
+  const [locations, setLocations] = useState([]);
+  const [loc, setLoc] = useState(locations[0]);
+  const [locIndex, setLocIndex] = useState(0);
 
   useEffect(() => {
+    if (plantList.length > 0) { // plantList에 값이 있다면
+      setLocations([plantList[0].ippt_gen, plantList[1].ippt_gen, plantList[2].ippt_gen]); // locations를 갱신합니다.
+    }
+  }, [plantList]);
+
+    useInterval(() => {
+    const nextIndex = (locIndex + 1) % locations.length;
+
+    setLoc(locations[nextIndex]);
+    setLocIndex(nextIndex);
+    // console.log(loc);
+  }, 2000);
+
+  // state 인포윈도우 content 상태
+  // const [contents, setContents] = useState('');
+  // useEffect(() => {
+  //   // loc가 변경될 때마다 contents를 갱신합니다.
+  //   if (loc !== null) {
+  //     setContents("<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px;'>광주광역시 동구</div>" +
+  //     "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
+
+  //     "<td style='width: 80px; color:#767676; padding-right:12px font-size: 18px;'><div style='font-size: 15px;'>발전량<div/></td>" +
+  //     "<td><span style='font-size: 15px; font-weight: bold;'>"+loc+"kw</span></td></tr>"+
+  //     +"</tbody></table>"
+  //     );
+  //   }
+  // }, [loc]); 
+
+  // component 지도 열기 -------------------------------
+  const mapMarkerInfoWindow = (plantList) => {
+
+    console.log(plantList);// 이 값을 넘어오게 하는 방법
     const { sop } = window;
     const map = sop.map("map"); //map 생성
-
+    
     const utmkXY = new sop.LatLng(36.24, 127.77); // 위도경도 -> utmk
     map.setView(sop.utmk(utmkXY.x, utmkXY.y), 2);// 지도 중심좌표로 뷰 설정
 
     map.dragging.disable(); // 지도 이동 불가
     map.scrollWheelZoom.disable(); // 지도 확대 불가
-
+    
     // 마커
     var myIcon = sop.icon({
-        iconUrl: 'https://i.ibb.co/D4FZbyp/marker.png',
-        iconSize:     [35, 35],
-        iconAnchor:   [22, 0],
-        shadowAnchor: [5, 0],
-        popupAnchor:  [-3, -76]
+      iconUrl: 'https://i.ibb.co/D4FZbyp/marker.png',
+      iconSize: [35, 35],
+      iconAnchor: [22, 0],
+      shadowAnchor: [5, 0],
+      popupAnchor: [-3, -76]
     });
-
+    console.log(plantList);
     const utmkXYm = new sop.LatLng(35.14627776, 126.9230903);
-    const marker = sop.marker([utmkXYm.x, utmkXYm.y], {icon: myIcon});
+    const marker = sop.marker([utmkXYm.x, utmkXYm.y], { icon: myIcon });
 
     // 인포윈도우 좌표 찍기 위도경도 -> utmk
     // 인포윈도우 생성
     const infoWindow = sop.infoWindow();
-
-    const contents = "<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px;'>광주광역시 동구 </div>" +
+    console.log(loc);
+    const contents = 
+      "<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px; text-align: center;'>"+plantList[0].dgen_ymd+"</div>" +
+      "<div style='font-size: 18px; font-weight: bold; margin-bottom: 5px;'>"+plantList[0].ippt+"</div>" +
       "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
 
       "<td style='width: 80px; color:#767676; padding-right:12px font-size: 18px;'><div style='font-size: 15px;'>발전량<div/></td>" +
-      "<td><span style='font-size: 15px; font-weight: bold;'>5000kw</span></td></tr>" 
-      +
+      "<td><span style='font-size: 15px; font-weight: bold;'>"+Math.floor(plantList[0].ippt_gen)+"MWh</span></td></tr>" +
 
-      "<tr><td style='color:#767676;padding-right:12px'><div style='font-size: 15px;'>전력단가<div/></td>" +
-      "<td><span style='font-size: 15px; font-weight: bold;'>000원</span></td></tr>" +
-
-      "<tr><td style='color:#767676;padding-right:12px'><div style='font-size: 15px;'>수익률<div/></td>" +
-      "<td style=''><span style='font-size: 15px; font-weight: bold;'>000원</span></td></tr>" +
-
-      "<tr><td style='color:#767676;padding-right:12px'><div style='font-size: 15px;'>투자비용<div/></td>" +
-      "<td><span style='font-size: 15px; font-weight: bold;'>000원</span></td>" +
-      "</tr></tbody></table>"
-      ;
+      "</tr></tbody></table>";
 
     infoWindow.setContent(contents);
 
-    marker.on("mouseover", function(e) {
+    marker.on("mouseover", function (e) {
       infoWindow.setUTMK([utmkXYm.x, utmkXYm.y]);
       infoWindow.openOn(map);
     });
-    marker.on("mouseout", function(e) {
+    marker.on("mouseout", function (e) {
       // infoWindow.close(map);
     });
     marker.addTo(map);
-
-    // 백엔드에서 발전소 이름, 발전량 가져오기
-    plantDeep();
-
-  }, []);
-
-
-  const plantDeep = () => {
-    let formData = new FormData();
-    axios
-    .get('/Sol/mapCon/sum',formData)
-    .then((res)=>{
-      console.log(res.data);
-      // setPlantList(res.data.data)
-
-    })
-    .catch((error) => {
-      console.log(error)
-    })
   }
 
-//   85G1 - 광양항세방태양광
-// 85M1 - 두산엔진MG태양광
-// 85S5 - 구미태양광
-// 987A - 영흠태양광 #3
-// 9878 - 예천태양광
-// C005 - 경상대태양광
 
+  // highcharts를 그리기 전 콤마 생성
+  Highcharts.setOptions({
+    lang: {
+      thousandsSep: ','
+    }
+  });
 
-  // --------------------  마커 인포메이션  ------------------
+  // plantList에 값이 있을 때 hightchart 옵션 생성----------------------------
+  useEffect(() => {
+    if (plantList.length > 0) {  
+      setOptions({
+        // 여기에 원래 options에 있던 내용을 그대로 복사해서 붙여넣으세요.
+        // 단, series 부분에서 plantList를 이용해 data를 생성하도록 변경하세요.
 
-  //  test 인포메이션 
-  // state 예측 발전량
-  const [count, setCount] = useState(0);
-
-  // state 예측 수익
-  const [revenue, setRevenue] = useState(0);
-
-  // state 발전소 이름
-  const locations = ['광주광역시 동구', '부산광역시 남구', '대구광역시 서구'];
-  const [loc, setLoc] = useState(locations[0]);
-  const [locIndex, setLocIndex] = useState(0);
-
-  useInterval(() => {
-    setCount(count + 1);
-    setRevenue(revenue + 1);
-
-    const nextIndex = (locIndex + 1) % locations.length;
-    // console.log(locIndex+1);
-    // console.log(locations.length);
-    // console.log((locIndex + 1) % locations.length);
-    setLoc(locations[nextIndex]);
-    setLocIndex(nextIndex);
-
-  }, 2000);
-
-
-  // 우측 꺾은선 그래프 발전량 예측 인포메이션
-  const options = {
-    title: {
-      text: '발전량 예측',
-      align: 'center'
-    },
-
-    yAxis: {
-      title: {
-        text: ''
-      }
-    },
-
-    xAxis: {
-      categories:[
-        '6','7','8','9','10','11','12','13','14','15','16'
-      ]
-
-    },
-    // 워터마크 해제
-    credits: {
-      enabled: false,
-    },
-    legend: {
-      layout: 'horizontal',
-      align: 'center',
-      verticalAlign: 'bottom'
-    },
-
-    plotOptions: {
-      series: {
-        label: {
-          connectorAllowed: false
-        },
-        pointStart: 0
-      }
-    },
-
-    series: [{
-      name: '광주광역시 동구',
-      data: [10000, 20000, 25000, 40000, 35000, 60000,
-        55000, 40000, 45000, 20000, 10000]
-    }, {
-      name: '부산광역시 서구',
-      data: [20000, 40000, 35000, 80000, 70000, 120000,
-        110000, 80000, 90000, 40000, 20000]
-    }, {
-      name: '대전광역시 서구',
-      data: [15000, 30000, 35000, 60000, 55000, 90000,
-        85000, 60000, 65000, 30000, 15000]
-    }, {
-      name: '울산광역시 남구',
-      data: [20000, 30000, 20000, 60000, 55000, 90000, 55000,
-        60000, 40000, 30000, 15000]
-    }, {
-      name: '인천광역시 서구',
-      data: [25000, 50000, 55000, 100000, 95000, 150000,
-        145000, 100000, 105000, 50000, 45000]
-    }],
-
-
-    responsive: {
-      rules: [{
-        condition: {
-          maxWidth: 400
-        },
-        chartOptions: {
+          title: {
+            text: '발전량 예측',
+            align: 'center'
+          },         
+      
+          yAxis: {
+            title: {
+              text: 'MWh',
+            },
+            labels : {
+              format: '{value:,.0f}', 
+            }
+          },
+      
+          xAxis: {
+            categories: [
+              '21', '22', '23', '24', '25', '26', '27', '28', '01', '02', '03'
+            ]
+      
+          },
+          // 워터마크 해제
+          credits: {
+            enabled: false,
+          },
           legend: {
             layout: 'horizontal',
-            align: 'left',
+            align: 'center',
             verticalAlign: 'bottom'
+          },
+      
+          plotOptions: {
+            series: {
+              label: {
+                connectorAllowed: false
+              },
+              pointStart: 0
+            }
+          },
+      
+          series: [
+            {
+            name: plantList[0].ippt,
+            data: [plantList[0].ippt_gen, plantList[1].ippt_gen, plantList[2].ippt_gen, plantList[3].ippt_gen, plantList[4].ippt_gen,
+            plantList[5].ippt_gen, plantList[6].ippt_gen, plantList[7].ippt_gen, plantList[8].ippt_gen, plantList[9].ippt_gen]
+          },
+           {
+            name: plantList[10].ippt,
+            data: [plantList[10].ippt_gen,plantList[11].ippt_gen,plantList[12].ippt_gen,plantList[13].ippt_gen,plantList[14].ippt_gen,
+            plantList[15].ippt_gen,plantList[16].ippt_gen,plantList[17].ippt_gen,plantList[18].ippt_gen,plantList[19].ippt_gen]
           }
-        }
-      }]
+          // , {
+          //   name: '대전광역시 서구',
+          //   data: [15000, 30000, 35000, 60000, 55000, 90000,
+          //     85000, 60000, 65000, 30000, 15000]
+          // }, {
+          //   name: '울산광역시 남구',
+          //   data: [20000, 30000, 20000, 60000, 55000, 90000, 55000,
+          //     60000, 40000, 30000, 15000]
+          // }, {
+          //   name: '인천광역시 서구',
+          //   data: [25000, 50000, 55000, 100000, 95000, 150000,
+          //     145000, 100000, 105000, 50000, 45000]
+          // }
+        ],     
+          responsive: {
+            rules: [{
+              condition: {
+                maxWidth: 400
+              },
+              chartOptions: {
+                legend: {
+                  layout: 'horizontal',
+                  align: 'left',
+                  verticalAlign: 'bottom'
+                }
+              }
+            }]
+          }
+        
+      });
     }
-  };
+  }, [plantList]);  // plantList가 갱신될 때마다 이 훅을 실행
 
 
   return (
     <div className='map-container'>
-      <div id="map" style={{ width: '100%', height: '853px'}} />
+      <div id="map" style={{ width: '100%', height: '853px' }} />
       <div className='map-info-container'>
         <div className='map-info-box'>
           <div className='map-info-content'>
-
-
-            {/* <div className='map-info-content-loc'>{loc}</div>
-            <div className='map-info-content-power'>
-              <div className='mypage-right-icon-box'>
-              <div className='icon sun-icon'></div>
-              </div>
-              <div className='map-info-content-power-key'>{'발전량'}</div>
-              <div className='map-info-content-power-value'>{count}{'kw'}</div>
-            </div>
-
-            <div className='map-info-content-revenu'>
-              <div className='mypage-right-icon-box'>
-                <div className='icon dollar-coin-icon'></div>
-              </div>
-              <div className='map-info-content-revenu-key'>{'예상수익'}</div>
-              <div className='map-info-content-revenu-value'>{revenue}{'원'}</div>
-            </div> */}
-
             <HighchartsReact highcharts={Highcharts} options={options} />
           </div>
         </div>
