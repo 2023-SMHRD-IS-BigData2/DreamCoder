75c46e80729094b5c425ed11dbd46822d778d520
장서영 <145406855+seoyoung430@users.noreply.github.com>
2024-02-27 16:52:35 +0900

    지도 완료

--- Soleilect/src/main/frontend/src/views/Map/Map.jsx
+++ Soleilect/src/main/frontend/src/views/Map/Map.jsx
@@ -19,7 +19,7 @@ const Map = () => {
       .get('/Sol/mapCon/sum', formData)
       .then((res) => {
         setPlantList(res.data.data)
-
+        console.log(res.data.data);
         // 지도 여는 함수 실행
         mapMarkerInfoWindow(res.data.data);
       })
@@ -40,13 +40,13 @@ const Map = () => {
     }
   }, [plantList]);
 
-  useInterval(() => {
-    const nextIndex = (locIndex + 1) % locations.length;
+  // useInterval(() => {
+  //   const nextIndex = (locIndex + 1) % locations.length;
 
-    setLoc(locations[nextIndex]);
-    setLocIndex(nextIndex);
-    // console.log(loc);
-  }, 2000);
+  //   setLoc(locations[nextIndex]);
+  //   setLocIndex(nextIndex);
+  //   console.log(loc);
+  // }, 2000);
 
 
   // component 지도 열기 -------------------------------
@@ -96,72 +96,78 @@ const Map = () => {
     const utmkXYm6 = new sop.LatLng(35.158898, 128.090460);
     const marker6 = sop.marker([utmkXYm6.x, utmkXYm6.y], { icon: myIcon });
     
+    // 85G1 - 광양항세방태양광 34.911494 / 127.679291
+    // 85M1 - 두산엔진MG태양광 35.214112 / 128.631138
+    // 85S5 - 구미태양광 36.134421 / 128.448690
+    // 987A - 영흠태양광 #3 37.240875 /  126.4372226
+    // 9978 - 예천태양광 36.752973 / 128.426090
+    // C005 - 경상대태양광 35.158898 / 128.090460
 
     // -----------------------------광양항세방태양광 --------------------------
     const contents1 =
-      "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[0].dgen_ymd + "</div>" +
+      "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[7].dgen_ymd + "</div>" +
       "<div style='font-size: 15px;  margin-bottom: 5px;  font-weight: bold; color : rgb(51, 51, 51);text-align: center;'>광양항 세방 태양광</div>" +
       "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
 
       "<td style='width: 80px; color : rgb(110,110,110); padding-right:14px font-size: 18px; font-weight: bold;'><div style='font-size: 15px;'>발전량<div/></td>" +
-      "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[0].ippt_gen) + "MWh</span></td></tr>" +
+      "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[7].ippt_gen) + "MWh</span></td></tr>" +
 
       "</tr></tbody></table>";
 
 
     // -----------------------------두산엔진 MG태양광  --------------------------
     const contents2 =
-      "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[0].dgen_ymd + "</div>" +
+      "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[59].dgen_ymd + "</div>" +
       "<div style='font-size: 15px;  margin-bottom: 5px;  font-weight: bold; color : rgb(51, 51, 51);text-align: center;'>두산엔진 MG태양광 </div>" +
       "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
 
       "<td style='width: 80px; color : rgb(110,110,110); padding-right:14px font-size: 18px; font-weight: bold;'><div style='font-size: 15px;'>발전량<div/></td>" +
-      "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[0].ippt_gen) + "MWh</span></td></tr>" +
+      "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[59].ippt_gen) + "MWh</span></td></tr>" +
 
       "</tr></tbody></table>";
 
 
     // -----------------------------구미 태양광  --------------------------
     const contents3 =
-      "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[0].dgen_ymd + "</div>" +
+      "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[20].dgen_ymd + "</div>" +
       "<div style='font-size: 15px;  margin-bottom: 5px;  font-weight: bold; color : rgb(51, 51, 51);text-align: center;'>구미 태양광 </div>" +
       "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
 
       "<td style='width: 80px; color : rgb(110,110,110); padding-right:14px font-size: 18px; font-weight: bold;'><div style='font-size: 15px;'>발전량<div/></td>" +
-      "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[0].ippt_gen) + "MWh</span></td></tr>" +
+      "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[20].ippt_gen) + "MWh</span></td></tr>" +
 
       "</tr></tbody></table>";
 
           // -----------------------------영흠태양광  --------------------------
     const contents4 =
-    "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[0].dgen_ymd + "</div>" +
+    "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[46].dgen_ymd + "</div>" +
     "<div style='font-size: 15px;  margin-bottom: 5px;  font-weight: bold; color : rgb(51, 51, 51);text-align: center;'>영흠태양광 </div>" +
     "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
 
     "<td style='width: 80px; color : rgb(110,110,110); padding-right:14px font-size: 18px; font-weight: bold;'><div style='font-size: 15px;'>발전량<div/></td>" +
-    "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[0].ippt_gen) + "MWh</span></td></tr>" +
+    "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[46].ippt_gen) + "MWh</span></td></tr>" +
 
     "</tr></tbody></table>";
 
         // -----------------------------예천태양광  --------------------------
         const contents5 =
-        "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[0].dgen_ymd + "</div>" +
+        "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[33].dgen_ymd + "</div>" +
         "<div style='font-size: 15px;  margin-bottom: 5px;  font-weight: bold; color : rgb(51, 51, 51);text-align: center;'>예천태양광 </div>" +
         "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
   
         "<td style='width: 80px; color : rgb(110,110,110); padding-right:14px font-size: 18px; font-weight: bold;'><div style='font-size: 15px;'>발전량<div/></td>" +
-        "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[0].ippt_gen) + "MWh</span></td></tr>" +
+        "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[33].ippt_gen) + "MWh</span></td></tr>" +
   
         "</tr></tbody></table>";
 
             // -----------------------------경상대태양광  --------------------------
     const contents6 =
-    "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[0].dgen_ymd + "</div>" +
+    "<div style='font-size: 18px; font-weight: bolder; margin-bottom: 5px; text-align: center;'>" + plantList[72].dgen_ymd + "</div>" +
     "<div style='font-size: 15px;  margin-bottom: 5px;  font-weight: bold; color : rgb(51, 51, 51);text-align: center;'>경상대태양광 </div>" +
     "<table style='border-spacing: 2px; border: 0px'><tbody><tr>" +
 
     "<td style='width: 80px; color : rgb(110,110,110); padding-right:14px font-size: 18px; font-weight: bold;'><div style='font-size: 15px;'>발전량<div/></td>" +
-    "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[0].ippt_gen) + "MWh</span></td></tr>" +
+    "<td><span style='font-size: 15px; color:rgba(0,0,0,0.9); font-weight: bold;'>" + Math.floor(plantList[72].ippt_gen) + "MWh</span></td></tr>" +
 
     "</tr></tbody></table>";
 
@@ -205,8 +211,8 @@ const Map = () => {
           labels: {
             format: '{value:,.0f}',
           },
-          max:9000,
-          min:5000
+          // max:9000,
+          // min:5000
         },
 
         xAxis: {
@@ -233,56 +239,37 @@ const Map = () => {
             pointStart: 0
           }
         },
-            // 85G1 - 광양항세방태양광 34.911494 / 127.679291
-    // 85M1 - 두산엔진MG태양광 35.214112 / 128.631138
-    // 85S5 - 구미태양광 36.134421 / 128.448690
-    // 987A - 영흠태양광 #3 37.240875 /  126.4372226
-    // 9978 - 예천태양광 36.752973 / 128.426090
-    // C005 - 경상대태양광 35.158898 / 128.090460
-        series: [{
-          name: '광양항세방태양광',
-          data: [6022, 6954, 7089, 7301, 7062, 6418, 6723, 6167, 6701, 7378]
-        }, {
-          name: '두산엔진MG태양광',
-          data: [6692, 6432, 6236, 7259, 6481, 7068, 7373, 7370, 6635, 7302]
-        }, {
-          name: '구미태양광',
-          data: [6546, 6046, 6951, 7345, 6486, 6232, 6528, 6270, 7395, 6651]
-        }, {
-          name: '영흠태양광',
-          data: [6714, 7374, 7184, 6059, 6426, 7316, 6287, 6855, 7214, 7491]
-        }, {
-          name: '예천태양광',
-          data: [7145, 6315, 7331, 7410, 7368, 6786, 7050, 6180, 7127, 7082]
-        }, {
-          name: '경상대태양광',
-          data: [6718, 7169, 6157, 6809, 7151, 6437, 6458, 6398, 7393, 7395]
-        }],
-        // series: [
-        //   {
-        //     name: plantList[0].ippt,
-        //     data: [plantList[0].ippt_gen, plantList[1].ippt_gen, plantList[2].ippt_gen, plantList[3].ippt_gen, plantList[4].ippt_gen,
-        //     plantList[5].ippt_gen, plantList[6].ippt_gen, plantList[7].ippt_gen, plantList[8].ippt_gen, plantList[9].ippt_gen]
-        //   },
-        //   {
-        //     name: plantList[10].ippt,
-        //     data: [plantList[10].ippt_gen, plantList[11].ippt_gen, plantList[12].ippt_gen, plantList[13].ippt_gen, plantList[14].ippt_gen,
-        //     plantList[15].ippt_gen, plantList[16].ippt_gen, plantList[17].ippt_gen, plantList[18].ippt_gen, plantList[19].ippt_gen]
-        //   }
-        //   // , {
-        //   //   name: '대전광역시 서구',
-        //   //   data: [15000, 30000, 35000, 60000, 55000, 90000,
-        //   //     85000, 60000, 65000, 30000, 15000]
-        //   // }, {
-        //   //   name: '울산광역시 남구',
-        //   //   data: [20000, 30000, 20000, 60000, 55000, 90000, 55000,
-        //   //     60000, 40000, 30000, 15000]
-        //   // }, {
-        //   //   name: '인천광역시 서구',
-        //   //   data: [25000, 50000, 55000, 100000, 95000, 150000,
-        //   //     145000, 100000, 105000, 50000, 45000]
-        //   // }
-        // ],
+            
+
+        series: [
+          {
+            name: '광양항세방태양광',
+            data: [plantList[3].ippt_gen, plantList[4].ippt_gen, plantList[5].ippt_gen, plantList[6].ippt_gen, plantList[7].ippt_gen,
+            plantList[8].ippt_gen, plantList[9].ippt_gen, plantList[10].ippt_gen, plantList[11].ippt_gen, plantList[12].ippt_gen]
+          },
+          {
+            name: '구미태양광',
+            data: [plantList[16].ippt_gen, plantList[17].ippt_gen, plantList[18].ippt_gen, plantList[19].ippt_gen, plantList[20].ippt_gen,
+            plantList[21].ippt_gen, plantList[22].ippt_gen, plantList[23].ippt_gen, plantList[24].ippt_gen, plantList[25].ippt_gen]
+          },{
+            name: '예천태양광',
+            data: [plantList[29].ippt_gen, plantList[30].ippt_gen, plantList[31].ippt_gen, plantList[32].ippt_gen, plantList[33].ippt_gen,
+            plantList[34].ippt_gen, plantList[35].ippt_gen, plantList[36].ippt_gen, plantList[37].ippt_gen, plantList[38].ippt_gen]
+          },{
+            name: '영흠태양광',
+            data: [plantList[42].ippt_gen, plantList[43].ippt_gen, plantList[44].ippt_gen, plantList[45].ippt_gen, plantList[46].ippt_gen,
+            plantList[47].ippt_gen, plantList[48].ippt_gen, plantList[49].ippt_gen, plantList[50].ippt_gen, plantList[51].ippt_gen]
+          },{
+            name: '두산엔진MG태양광',
+            data: [plantList[55].ippt_gen, plantList[56].ippt_gen, plantList[57].ippt_gen, plantList[58].ippt_gen, plantList[59].ippt_gen,
+            plantList[60].ippt_gen, plantList[61].ippt_gen, plantList[62].ippt_gen, plantList[63].ippt_gen, plantList[64].ippt_gen]
+          },{
+            name: '경상대태양광',
+            data: [plantList[68].ippt_gen, plantList[69].ippt_gen, plantList[70].ippt_gen, plantList[71].ippt_gen, plantList[72].ippt_gen,
+            plantList[73].ippt_gen, plantList[74].ippt_gen, plantList[75].ippt_gen, plantList[76].ippt_gen, plantList[77].ippt_gen]
+          }
+
+        ],
         responsive: {
           rules: [{
             condition: {
